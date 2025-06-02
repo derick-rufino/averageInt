37b63d1674167aff3843b8bb68f71e03376b1bcd
derick-rufino <derick10011@gmail.com>
2025-06-02 16:29:20 -0300

    Funcionalidades primárias em construção
    -  JS: função para aleatorizar 4 números diferentes e associar às caixas
    - variável média não está funcionando como Number
    - função para armazenar user input criada
    - HTML e CSS: botões agora tem fundo e borda removidas por padrão
    - grid da main expandida em 2 colunas (totalizando 10)
    - nova seção: extra features (inclui botão de dica, display de pontuação e timer) (é exibida apenas no modo diícil, por isso, ainda está em construção)

---
+++ app.js
@@ -0,0 +1,42 @@
+// Variáveis globais
+// Number Displays
+let num1 = document.getElementById('number1');
+let num2 = document.getElementById('number2');
+let num3 = document.getElementById('number3');
+let num4 = document.getElementById('number4');
+let media = null;
+
+// Botões
+let randomizeBtn = document.getElementById('randomize-btn')
+
+
+randomizeBtn.addEventListener("click", function generateRandom(){
+    console.clear(); //limpa a tentativa anterior
+    num1.innerHtml = parseInt(Math.random()*20);
+    num2.innerHtml = parseInt(Math.random()*20);
+    num3.innerHtml = parseInt(Math.random()*20);
+    num4.innerHtml = parseInt(Math.random()*20);
+
+    media = (num1 + num2 + num3 + num4)/4;
+    
+    // for(i = 0; i < 4; i++){
+    //   console.log(parseInt(Math.random()*20))
+    // }
+    console.log("A média é: " + media)
+});
+
+// Armazenando a entrada do usuário
+const guessForm = document.getElementById('guessForm');
+
+guessForm.addEventListener('submit', (e) => {
+    e.preventDefault(); //previne que a página recarregue ao enviar o form
+    const formValue = new FormData(guessForm);
+    
+    for (item of formValue){ //para cada item dentro dos valores do form
+        console.log(item) //loga o item que o user deu entrada em formato array
+        let userGuessValue = item[1]; //acessa o array e armazena apenas o valor numérico
+        console.log("User Input: " + userGuessValue);
+
+        document.getElementById('userGuess').value = "";
+    }
+});

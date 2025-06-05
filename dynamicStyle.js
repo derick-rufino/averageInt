dbe804859bf6fa277cb9976b4304434f20e35aa9
derick-rufino <derick10011@gmail.com>
2025-06-05 19:22:23 -0300

    Novo Layout Mobile
    - Layout em coluna
    - Seleção de modo e card de pontuação agora são pop-ups

--- dynamicStyle.js
+++ dynamicStyle.js
@@ -7,6 +7,7 @@ let uMessage = document.getElementById("message");
 
 const rankingCard = document.getElementById("rankingCard");
 const gameModeCard = document.querySelector(".gameMode-card");
+const coverAll = document.querySelector(".coverAll");
 
 let currentPoints = document.getElementById("currentPoints");
 
@@ -22,6 +23,7 @@ function closeModal(clickedBtn) {
   const modal = clickedBtn.closest("section, .modal, .cardInfo");
   if (modal) {
     modal.style.display = "none";
+    coverAll.style.display = "none";
   }
 }
 
@@ -29,13 +31,14 @@ function closeModal(clickedBtn) {
 function openModal(modal) {
   if (modal) {
     modal.style.display = "grid";
+    coverAll.style.display = "block";
   }
 }
 
 // Seleciona todos os botões que abrem modais
-const openModalBtns = document.querySelectorAll('.openModalBtn');
-openModalBtns.forEach(btn => {
-  btn.addEventListener('click', function() {
+const openModalBtns = document.querySelectorAll(".openModalBtn");
+openModalBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
     // Decide qual modal abrir com base no botão clicado
     if (this.id === "rankingCard-btn") {
       openModal(document.getElementById("rankingCard"));
@@ -44,3 +47,14 @@ openModalBtns.forEach(btn => {
     }
   });
 });
+
+function atualizarDisplayModo() {
+  const display = document.getElementById("currentMode-display");
+  let textoModo = "";
+  if (currentMode === "1") textoModo = "Aprendiz";
+  else if (currentMode === "2") textoModo = "Normal";
+  else if (currentMode === "3") textoModo = "Médio";
+  else if (currentMode === "4") textoModo = "Difícil";
+  else textoModo = "Normal";
+  display.innerText = textoModo;
+}
